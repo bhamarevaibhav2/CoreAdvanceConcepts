3428f649e7757e771a2350bd006640980422dcf8
Vaibhav Bhamare <bhamarevaibhav2@gmail.com>
2024-08-04 18:47:55 +0530

    Design pattern pkg

--- src/com/mgshome/designPatterns/PrincipalMain.java
+++ src/com/mgshome/designPatterns/PrincipalMain.java
@@ -6,9 +6,9 @@ public class PrincipalMain {
         //new Principal(); we can not create object of class principle because constructor is private
 
         Principal principal = Principal.getPrincipal(); //we can call method using class name because method is static
-        System.out.println(principal.hashCode());
+        System.out.println(principal.hashCode()); //460141958
         //same object will be return because method will call all time but in method there is condition
         Principal principal2 = Principal.getPrincipal();
-        System.out.println(principal2.hashCode());
+        System.out.println(principal2.hashCode()); //460141958
     }
 }
